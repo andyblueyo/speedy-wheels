388e94b3192c6fc3d107b2d73f04e3c5397823bc
yichiang <yi_chiang@outlook.com>
2017-09-23 15:55:04 -0700

    add map

--- src/App.js
+++ src/App.js
@@ -3,13 +3,38 @@ import { Card, Image, Icon, Container } from 'semantic-ui-react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.css'
 import 'semantic-ui-css/semantic.min.css';
-
+import { Map, Marker, Popup, TileLayer } from 'react-leaflet';
+const position = [51.505, -0.09];
+const map = (
+  <Map center={position} zoom={13}>
+    <TileLayer
+      url='http://{s}.tile.osm.org/{z}/{x}/{y}.png'
+      attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+    />
+    <Marker position={position}>
+      <Popup>
+        <span>A pretty CSS3 popup.<br/>Easily customizable.</span>
+      </Popup>
+    </Marker>
+  </Map>
+);
 class App extends Component {
 
   render() {
 
     return (
       <Container>
+      <Map center={position} zoom={13}>
+        <TileLayer
+          url='http://{s}.tile.osm.org/{z}/{x}/{y}.png'
+          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        />
+        <Marker position={position}>
+          <Popup>
+            <span>A pretty CSS3 popup.<br/>Easily customizable.</span>
+          </Popup>
+        </Marker>
+      </Map>
       <Card>
        <Image src='http://www.kirklandwa.gov/Assets/Senior+Center+Van.jpg' />
        <Card.Content>
